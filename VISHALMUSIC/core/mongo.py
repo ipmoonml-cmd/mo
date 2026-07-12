cfa0e212bbb92375085439d108c0888ee3c567a9
ipmoonml-cmd <ip.moon.ml@gmail.com>
2026-07-12 21:26:37 +0300

    Add files via upload

---
+++ VISHALMUSIC/core/mongo.py
@@ -0,0 +1,25 @@
+# ═══════════════════════════════════════════════════════════
+#        😎  VISHAL MUSIC BOT  😎
+#   GitHub : github.com/ItsMeVishal0/VishalMusic
+#   Developer : @ItsMeVishalBots | Telegram
+#   Module : MongoDB Database Connection
+# ═══════════════════════════════════════════════════════════
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_DB_URI
+from ..logging import LOGGER
+
+LOGGER(__name__).info("Connecting to your Mongo Database...")
+
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI, serverSelectionTimeoutMS=5000)
+    mongodb = _mongo_async_.Vishal
+    LOGGER(__name__).info("Connected to your Mongo Database.")
+except Exception as e:
+    LOGGER(__name__).error(f"Failed to connect to your Mongo Database: {e}")
+    exit()
+
+# ═══════════════════════════════════════════════════════════
+#        😎  VISHAL MUSIC BOT  😎
+#   github.com/ItsMeVishal0/VishalMusic
+# ═══════════════════════════════════════════════════════════
